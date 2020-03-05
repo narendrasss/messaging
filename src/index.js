f7a1cae0a16cd1accb0ce09f3cf408b2c99d59f3
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-03-04 20:48:07 -0800

    Refactor to use node-messenger sdk

--- src/index.js
+++ src/index.js
@@ -1,126 +1,53 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
+const Messenger = require("messenger-node");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 9000;
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
-const app = express().use(bodyParser.json());
-
-app.get("/webhook", (req, res) => {
-  const VERIFY_TOKEN = "HELLO";
-
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
-
-  if (mode && token) {
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      console.log("Verified webhook");
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);
-    }
-  } else {
-    res.sendStatus(400);
-  }
+const Webhook = new Messenger.Webhook({
+  verify_token: process.env.VERIFY_TOKEN
 });
 
-app.post("/webhook", (req, res) => {
-  const { body } = req;
-  if (body.object === "page") {
-    body.entry.forEach(entry => {
-      const webhookEvent = entry.messaging[0];
-      console.log(webhookEvent);
-
-      const senderId = webhookEvent.sender.id;
-      console.log("Sender PSID: " + senderId);
-
-      if (webhookEvent.message) {
-        handleMessage(senderId, webhookEvent.message);
-      } else if (webhookEvent.postback) {
-        handlePostback(senderId, webhookEvent.postback);
-      }
-    });
-    res.status(200).send("Event received");
-  } else {
-    res.sendStatus(404);
-  }
+const Client = new Messenger.Client({
+  page_token: process.env.PAGE_ACCESS_TOKEN
 });
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-
-function handleMessage(senderId, receivedMessage) {
-  let response;
-  if (receivedMessage.text) {
-    response = {
-      text: `You said: ${receivedMessage.text}`
-    };
-  } else if (receivedMessage.attachments) {
-    const url = receivedMessage.attachments[0].payload.url;
-    response = {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [
+Webhook.on("messages", (_, sender_info, webhook_event) => {
+  const { message } = webhook_event;
+  const recipient = { id: sender_info.value };
+  if (message.text) {
+    Client.sendText(recipient, webhook_event.message.text);
+  } else if (message.attachments) {
+    const { url } = message.attachments[0].payload;
+    const template = {
+      template_type: "generic",
+      elements: [
+        {
+          title: "Is this the right picture?",
+          subtitle: "Tap a button to answer.",
+          image_url: url,
+          buttons: [
             {
-              title: "Is this the right picture?",
-              subtitle: "Tap a button to answer.",
-              image_url: url,
-              buttons: [
-                {
-                  type: "postback",
-                  title: "Yes!",
-                  payload: "yes"
-                },
-                {
-                  type: "postback",
-                  title: "No!",
-                  payload: "no"
-                }
-              ]
+              type: "postback",
+              title: "Yes!",
+              payload: "yes"
+            },
+            {
+              type: "postback",
+              title: "No!",
+              payload: "no"
             }
           ]
         }
-      }
+      ]
     };
+    Client.sendTemplate(recipient, template);
   }
-  sendMessage(senderId, response);
-}
+});
 
-function handlePostback(senderId, receivedPostback) {
-  let response;
-  const { payload } = receivedPostback;
-  if (payload === "yes") {
-    response = { text: "Thanks!" };
-  } else if (payload === "no") {
-    response = { text: "Oops, send me another image." };
+Webhook.on("messaging_postbacks", (_, sender_info, webhook_event) => {
+  const { postback } = webhook_event;
+  const recipient = { id: sender_info.value };
+  if (postback.payload === "yes") {
+    Client.sendText(recipient, "Thanks!");
+  } else if (postback.payload === "no") {
+    Client.sendText(recipient, "Oops, send me another picture.");
   }
-  sendMessage(senderId, response);
-}
-
-function sendMessage(senderId, response) {
-  const body = {
-    recipient: {
-      id: senderId
-    },
-    message: response
-  };
-  request(
-    {
-      uri: "https://graph.facebook.com/v2.6/me/messages",
-      qs: { access_token: PAGE_ACCESS_TOKEN },
-      method: "POST",
-      json: body
-    },
-    err => {
-      if (err) {
-        console.error(`Unable to send message: ${err}`);
-      } else {
-        console.log("Successfuly sent message");
-      }
-    }
-  );
-}
+});

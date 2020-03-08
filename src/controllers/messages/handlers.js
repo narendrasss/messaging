cabe7b387aeec87e22b62e485583e2ce0d4b973c
Narendra Syahrasyad <31267630+narendrasss@users.noreply.github.com>
2020-03-07 16:50:04 -0800

    Parse marketplace listing (#4)
    * Implement firebase integration
    
    * Update readme
    
    * Add comments for clarification
    
    * Refactor db logic
    
    * Refactor controllers folder structure
    
    * Parse listing and add to db

---
+++ src/controllers/messages/handlers.js
@@ -0,0 +1,68 @@
+const { db } = require("../../db");
+const { getListingId } = require("./helpers");
+
+function handleText(client, recipient, message) {
+  client.sendText(recipient, message.text);
+}
+
+function handleDebug(client, recipient, message) {
+  client.sendTemplate(recipient, {
+    template_type: "button",
+    text: "DEBUG",
+    buttons: [
+      {
+        type: "postback",
+        title: "Get started",
+        payload: "get-started"
+      }
+    ]
+  });
+}
+
+function handleAttachments(client, recipient, message) {
+  const { url } = message.attachments[0].payload;
+  const template = {
+    template_type: "generic",
+    elements: [
+      {
+        title: "Is this the right picture?",
+        subtitle: "Tap a button to answer.",
+        image_url: url,
+        buttons: [
+          {
+            type: "postback",
+            title: "Yes!",
+            payload: "yes"
+          },
+          {
+            type: "postback",
+            title: "No!",
+            payload: "no"
+          }
+        ]
+      }
+    ]
+  };
+  client.sendTemplate(recipient, template);
+}
+
+function handleListing(client, recipient, message) {
+  const listings = db.ref("listings");
+  const listingId = getListingId(message);
+  listings.child(listingId).once("value", snapshot => {
+    if (snapshot.val()) {
+      client.sendText(recipient, "You're a buyer!");
+    } else {
+      // TODO: Prompt the user if they are seller or not
+      listings.child(listingId).set({ seller: recipient.id });
+      client.sendText(recipient, "You're the seller!");
+    }
+  });
+}
+
+module.exports = {
+  handleAttachments,
+  handleDebug,
+  handleText,
+  handleListing
+};
---
+++ src/controllers/messages/helpers.js
@@ -0,0 +1,39 @@
+const messageTypes = {
+  DEBUG: "debug",
+  TEXT: "text",
+  MULTIPLE: "multiple",
+  LISTING: "listing",
+  ATTACHMENT: "attachment",
+  UNSUPPORTED: "unsupported"
+};
+
+function getMessageType(message) {
+  if (message.text) {
+    if (message.text.toLowerCase() === "debug") {
+      return messageTypes.DEBUG;
+    }
+    return messageTypes.TEXT;
+  }
+  if (message.attachments) {
+    if (message.attachments.length > 1) {
+      return messageTypes.MULTIPLE;
+    }
+    const payload = message.attachments[0].payload;
+    if (payload.url.includes("marketplace/item")) {
+      return messageTypes.LISTING;
+    }
+    return messageTypes.ATTACHMENT;
+  }
+  return messageTypes.UNSUPPORTED;
+}
+
+function getListingId(message) {
+  const { url } = message.attachments[0].payload;
+  const id = url.split("item/")[1];
+  if (id[id.length - 1] === "/") {
+    return id.slice(0, id.length - 1);
+  }
+  return id;
+}
+
+module.exports = { getMessageType, getListingId, messageTypes };

61056a2f7743bad593025ecea62d529f34bc249f
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-03-07 22:34:12 -0800

    Move firebase config to env files

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,14 @@
 const firebase = require("firebase-admin");
 
+const key = process.env.FIREBASE_PRIVATE_KEY;
+
 firebase.initializeApp({
-  credential: firebase.credential.applicationDefault(),
-  databaseURL: process.env.DB_URL
+  credential: firebase.credential.cert({
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: key.replace(/\\n/g, "\n"),
+    projectId: process.env.FIREBASE_PROJECT_ID
+  }),
+  databaseURL: process.env.FIREBASE_DB_URL
 });
 
 module.exports = {
